2f96555a8af23741ae79564160861c10213bc1d1
alovey1014 <wong.ying@graduate.utm.my>
2023-06-22 23:48:05 +0800

    Edit

--- Tetra_Project.cpp
+++ Tetra_Project.cpp
@@ -442,7 +442,7 @@ int main()
         cin >> phonenum;
         cout << "Do you have VIP membership? (Y/N): ";
         cin >> vip;
-        toupper(vip);
+        vip = toupper(vip);
 
         // input reservation details
         cout << "Number of pax: ";
@@ -452,7 +452,7 @@ int main()
         cout << "Time (eg: 1400): ";
         cin >> time;
         bool isVIP;
-        if (vip == 'y' || vip == 'Y')
+        if (vip == 'Y')
             isVIP = 1;
         else
             isVIP = 0;
